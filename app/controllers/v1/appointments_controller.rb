7da6339122fbbddb9845aa60dfb66a7cbc2faafa
olawale-o <omoogunolawale@gmail.com>
2021-12-01 16:10:03 +0100

    Authorize user can book appointment with doctor

--- app/controllers/v1/appointments_controller.rb
+++ app/controllers/v1/appointments_controller.rb
@@ -1,4 +1,5 @@
 class V1::AppointmentsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_appointment, only: %i[show destroy]
 
   def index
@@ -10,8 +11,9 @@ class V1::AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.new(appointment_params)
 
+    current_user.appointments << @appointment
     if @appointment.save
-      render json: @appointment, status: :created, location: @appointment
+      render json: @appointment, status: :created
     else
       render json: @appointment.errors, status: :unprocessable_entity
     end
