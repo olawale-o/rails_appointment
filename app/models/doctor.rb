1d2c9ba21a43c5f5009b3e7b9a9eff065c1a7522
olawale-o <omoogunolawale@gmail.com>
2021-12-01 08:41:23 +0100

    Add validations to doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ApplicationRecord
+  validates :fullname, :specialty, :picture, :city, presence: true
+  validates :fullname, uniqueness: { scope: :user_id }
   belongs_to :user
   has_many :appointments
 end
