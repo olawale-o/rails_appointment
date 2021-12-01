868e5a393ef1ba22d1718833e98cd6e5419afcf6
olawale-o <omoogunolawale@gmail.com>
2021-12-01 10:53:44 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  scope :v1, defaults: { format: :json } do
+    devise_for :users
+  end
+  namespace :v1, defaults: { format: :json } do
+    resources :doctors
+    resources :appointments
+  end
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
