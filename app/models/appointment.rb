8b8613135f5d660ebc00d2b115655910a8bd25f5
olawale-o <omoogunolawale@gmail.com>
2021-12-01 08:41:56 +0100

    Add validation to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
+  validates :book_for, presence: true
+  validates :book_for, uniqueness: { scope: %i[user_id doctor_id] }
+
   belongs_to :user
   belongs_to :doctor
 end
