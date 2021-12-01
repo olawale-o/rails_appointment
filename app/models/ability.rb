674ac002f3b5cc67c1195afb31e3e39d67e40afb
olawale-o <omoogunolawale@gmail.com>
2021-12-01 16:08:41 +0100

    Fix Ability bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,9 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :manage, Doctor, User: user
-    can :manage, Appointment, User: user
+    can :read, Doctor
+    can :manage, Doctor, user: user
+    can :manage, Appointment, user: user
     # can :manage, :all if user.admin?
   end
 end
