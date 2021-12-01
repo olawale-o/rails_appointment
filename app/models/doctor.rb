99148336351288c26c2f1454c319b7c2b4d1adc9
olawale-o <omoogunolawale@gmail.com>
2021-12-01 14:49:00 +0100

    Associate active storage with doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,8 @@
 class Doctor < ApplicationRecord
   validates :fullname, :specialty, :picture, :city, presence: true
   validates :fullname, uniqueness: { scope: :user_id }
+
   belongs_to :user
   has_many :appointments
+  has_one_attached :picture
 end
