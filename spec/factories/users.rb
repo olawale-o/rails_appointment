64ecbfadce2b5eb810663974efed9ad661bcfd19
olawale-o <omoogunolawale@gmail.com>
2021-12-02 09:11:34 +0100

    Upgrade users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,9 @@
 FactoryBot.define do
-  factory :user
+  sequence(:email) { |n| "test#{n}@email.com" }
+  factory :user do
+    email
+    name { 'realuser' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
 end
