888b8d10da13d4f7f0446976ce82044e00e671e9
olawale-o <omoogunolawale@gmail.com>
2021-12-14 15:57:22 +0100

    Allow frontend deploy link in cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3006', 'http://192.168.1.101:3006'
+    origins 'localhost:3006', 'http://192.168.1.101:3006', 'https://upbeat-hawking-cf5580.netlify.app/'
 
     resource '*',
       headers: :any,
